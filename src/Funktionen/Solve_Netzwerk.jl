2a9eaac3c528a633874c0d0e90cea45fa9c3346f
FerdinandRieck <ferdinand.rieck@smail.emt.h-brs.de>
2023-05-12 14:58:54 +0200

    #20

--- src/Funktionen/Solve_Netzwerk.jl
+++ src/Funktionen/Solve_Netzwerk.jl
@@ -2,8 +2,7 @@ function solveNetzwerk()
     println("---------------- This is FlexhyX ------------------")
 #-- Netwerk einlesen
     dir = dirname(@__DIR__)
-    dir = dir*"/Netzwerk/flexhyx.cfg"
-    J_cfg = JSON.parsefile(dir)
+    J_cfg = JSON.parsefile(dir*"/Netzwerk/flexhyx.cfg")
     now = Dates.now(); jetzt = [Dates.year(now) Dates.month(now) Dates.day(now) Dates.hour(now) Dates.minute(now) 0]
     startzeit = get(J_cfg,"Startzeit",jetzt)
     startzeit = String(Symbol(startzeit'))

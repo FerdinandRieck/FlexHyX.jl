52b7660c555131d68ecf4835f610fbe64b9d9bcc
FerdinandRieck <ferdinand.rieck@smail.emt.h-brs.de>
2023-05-12 21:56:54 +0200

    50

--- src/FlexHyX.jl
+++ src/FlexHyX.jl
@@ -33,8 +33,6 @@ module FlexHyX
     export solveNetzwerk
     export plotsol
 
-    #sol = solveNetzwerk(dir*"/src")
+    sol = solveNetzwerk(dir*"/src")
     #plotsol(sol)
 end
-
-sol = solveNetzwerk(dir*"/src")
